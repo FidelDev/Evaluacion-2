35170bedae5339200cdf0d8ba191fff1b791dc66
FidelDev <fidelrosales339@gmail.com>
2024-09-24 01:23:20 -0600

    prueba2

--- app/routers/router.js
+++ app/routers/router.js
@@ -6,9 +6,9 @@ const usuario = require('../controllers/Usuarios.controller.js');
 //controllers.js
 router.post('/api/usuario/create', usuario.create);
 router.get('/api/usuario/all', usuario.retrieveAllUsuario);
-router.get('/api/usuario/onebyid/:UserId', usuario.getUserById);//
-router.put('/api/usuario/update/:UserId', usuario.updateById);//
-router.delete('/api/usuario/delete/:userId', usuario.deleteById);//
+router.get('/api/usuario/onebyid/:id_usuario', usuario.getUserById);//
+router.put('/api/usuario/update/:id_usuario', usuario.updateById);//
+router.delete('/api/usuario/delete/:id_usuario', usuario.deleteById);//
 
 
 module.exports = router;
